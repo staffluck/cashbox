af295c1a15d7aaa08dc1b970b1b19efaf4cae0c3
agent47daun <agent47daun@gmail.com>
2022-04-20 21:03:36 +0400

    Основной функционал этапа 2. Генерация QRCode, ссылающийся на чек

--- main/views.py
+++ main/views.py
@@ -1,21 +1,37 @@
 from uuid import uuid4
 
-import pdfkit
 from django.template.loader import render_to_string
+from django.utils import timezone
+from django.db import connection
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
+import pdfkit
+import qrcode
 
 from .serializers import ChequeIdsSerializer
-from .models import Item
+from .models import Cheque, Item
+
+
+def generate_qrcode(url: str) -> str:
+    img = qrcode.make(url)
+    unique_name = uuid4()
+    file_path = f"qrcodes/{unique_name}.png"
+    img.save(f"media/{file_path}")
+    return file_path
 
 def generate_cheque(items: Item, template="receipt.html") -> str:
     final_sum = 0  # Раз SQL запрос и так выполнится для получения всех items(для шаблона), использовать aggregate нет смысла(если объектов не много)
     for item in items:
         final_sum += item.price
 
-    template_string = render_to_string(template, context={"items": items, "final_sum": final_sum})
+    now = timezone.now()
+    template_string = render_to_string(template, context={"item1": items[0], "item2": items[1], "final_sum": final_sum, "created_at": now})
     unique_name = uuid4()
-    pdfkit.from_string(template_string, f"media/cheques/{unique_name}.pdf")
+    file_path = f"cheques/{unique_name}.pdf"
+    pdfkit.from_string(template_string, f"media/{file_path}")
+
+    cheque = Cheque.objects.create(cheque=file_path)
+    return cheque
 
 
 class GenerateChequeAPIView(GenericAPIView):
@@ -26,6 +42,10 @@ class GenerateChequeAPIView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         items = Item.objects.filter(id__in=serializer.data["ids"])
-        generate_cheque(items)
+        cheque = generate_cheque(items)
+        url_to_cheque = request.build_absolute_uri(cheque.cheque.url)
+        qrcode_path = generate_qrcode(url_to_cheque)
+        cheque.qrcode = qrcode_path
 
-        return Response(serializer.data)
+        url_to_qrcode = request.build_absolute_uri(cheque.qrcode.url)
+        return Response(url_to_qrcode)
